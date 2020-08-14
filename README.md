6f2b9a0c3d4c6936aba5a694ef54ed62707b13a8
customwebtemplatedesigner <68409068+customwebtemplatedesigner@users.noreply.github.com>
2020-08-13 20:20:07 -0700

    312. intro to REST

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # YelpCamProj
  YelpCamp ! :)
+
+
+Restful Routes
+
+name        url         verb        desc
+======================================================
+INDEX       /dogs       GET         Display a list of all dog
+NEW         /dogs/new   GET         Display the form to create a new Dog
+CREATE      /dogs       POST        Add a new dog to the DB
+SHOW        /dogs/:id   GET         Shows all info about a specific dog
+
+
+
+REST - a way of mapping together HTTP routes and CRUD
+
+
+CREATE - insert new Object into DB
+
+READ - retreiving info on Object
+
+UPDATE - updating an Object
+
+DESTROY - removing an Object
--- app.js
+++ app.js
@@ -1,59 +1,86 @@
-const express = require('express');
-app = express();
-const bodyParser = require('body-parser')
+const
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    PORT = 3000;
+
+
+// Server Settings
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-const PORT = 3000;
-
 app.set("view engine", "ejs");
 
+// Mongoose DB Connection
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-const images = [
-    "https://imgur.com/BHzefub.jpg",
-    "https://imgur.com/TSUTG8w.jpg",
-    "https://imgur.com/rEYa8Wi.jpg",
-    "https://imgur.com/WpOPYL3.jpg",
-    "https://imgur.com/AlTK3wZ.jpg"
-]
-let campgrounds = [{
-        name: "Salmon Creek",
-        image: images[0]
-    },
-    {
-        name: "Granite Hill",
-        image: images[1]
-    },
-    {
-        name: "Mountain Goat's Rest",
-        image: images[2]
-    },
-    {
-        name: "Big Dick Canyon",
-        image: images[3]
-    },
-    {
-        name: "Elder Crones Village",
-        image: images[4]
-    }
-]
+// Schema Setup
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+})
+
+
+
+// Campground DB Schema
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+
+// Campground.create({
+//     name: "Granite Hill",
+//     image: "https://imgur.com/BHzefub.jpg",
+//     description: "A beautiful paradise of does and deer."
+// })
+
+
+// Server Routes
+
+// INDEX -- Show all Campgrounds
 app.get("/campgrounds", function (req, res) {
-    res.render('campgrounds', {
-        campgrounds: campgrounds
+    console.log('getting all campgrounds')
+
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log("there was an error:")
+            console.log(err)
+        } else {
+            console.log("success! rendering campgrounds...")
+            res.render("index", {
+                campgrounds: allCampgrounds
+            })
+        }
     })
 })
 
+
+// CREATE -- Add new Campground to DB
 app.post("/campgrounds", function (req, res) {
-    
+
     let name = req.body.name
     let image = req.body.image
-    let newCampground = {name:name,image:image}
-    campgrounds.push(newCampground)
-    console.log("REDICTING!")
-    res.redirect('/campgrounds')
+    let description = req.body.description
+    let newCampground = {
+        name: name,
+        image: image,
+        description: description
+    }
+    Campground.create(newCampground, function (err, newlyCreated) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect('/campgrounds')
+        }
+    })
+
 
 })
 
+// NEW -- show form to create a new campground
 app.get("/campgrounds/new", function (req, res) {
     res.render('new')
 })
@@ -61,7 +88,24 @@ app.get("/", function (req, res) {
     res.render('landing')
 })
 
+// Find a specific Campground by ID,
+// Render 'Show' Template with the Campground
+app.get("/campgrounds/:id", function (req, res) {
 
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {
+                campground: foundCampground
+            })
+        }
+    })
+
+})
+
+
+// Server Start
 app.listen(PORT, function () {
     console.log("YELPCAMP SERVER LISTENING ON PORT: " + PORT)
 })
